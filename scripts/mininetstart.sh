7197b57876b89405b9fc857bbec74d12e3184c2c
YuriMzz <y.mazzuoli@studenti.unipi.it>
2022-01-05 16:30:36 +0100

    fix mininet start script

--- scripts/mininetstart.sh
+++ scripts/mininetstart.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 sudo mn --topo tree,2 --mac \
-#--switch default \ 
---switch ovsk --controller remote,ip=127.0.0.1,port=6653,protocols=OpenFlow13 
+--switch ovsk --controller remote,ip=127.0.0.1,port=6653,protocols=OpenFlow13\
 --ipbase=10.0.0.0
