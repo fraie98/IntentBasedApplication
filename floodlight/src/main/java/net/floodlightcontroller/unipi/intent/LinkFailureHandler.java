77f04c25230fc6c7f9ce36c42efd2a546e2e3ff0
fraie98 <francesco.iemma11@gmail.com>
2022-01-23 13:19:35 -0500

    Link Failure handler class added

---
+++ floodlight/src/main/java/net/floodlightcontroller/unipi/intent/LinkFailureHandler.java
@@ -0,0 +1,17 @@
+package net.floodlightcontroller.unipi.intent;
+
+import java.util.List;
+
+import net.floodlightcontroller.linkdiscovery.ILinkDiscovery.LDUpdate;
+import net.floodlightcontroller.topology.ITopologyListener;
+
+public class LinkFailureHandler implements ITopologyListener {
+
+	@Override
+	public void topologyChanged(List<LDUpdate> linkUpdates) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
