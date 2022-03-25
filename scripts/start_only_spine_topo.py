5483caccb764075c3aeddc5bb977289d3744a0ff
fraie98 <francesco.iemma11@gmail.com>
2022-03-25 13:49:27 -0400

    Spine leaf script modified

---
+++ scripts/start_only_spine_topo.py
@@ -0,0 +1,37 @@
+#!/usr/bin/sudo /usr/bin/python
+import os
+import sys
+
+from mininet.clean import Cleanup
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.cli import CLI
+from mininet.node import OVSSwitch,  RemoteController
+from mininet.link import TCLink
+from mininet.util import irange
+import requests
+
+from time import sleep
+from spine_leaf import dcSpineLeafTopo
+
+CONTROLLER_IP="127.0.0.1"
+CONTROLLER_PORT="8080"
+SPINES=2
+LEAFS=3
+N_HOSTS=2
+setLogLevel('info')
+print "cleanup junk from old runs"
+Cleanup.cleanup()
+print "Create remote controller to which switches are attached"
+rc = RemoteController( "c0", ip=CONTROLLER_IP,  protocols='OpenFlow13')
+topo = dcSpineLeafTopo(k=SPINES, l=LEAFS, n=N_HOSTS)
+net = Mininet(  topo=topo, link=TCLink,build=False, controller=rc )
+print "building network"
+net.build()
+print "starting network"
+net.start()
+
+print "Dumping host connections"
+dumpNodeConnections(net.hosts)
+CLI(net)
